c2fb4e9c20ae4f81769ce525a2f8cf1974b9c39b
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-27 18:07:09 -0500

    made add user work - need to add validation in form

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay6.Models;
+
+namespace SpaDay6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.username = newUser.Username;
+                return View("index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match! Try again!";
+                ViewBag.userName = newUser.Username;
+                ViewBag.eMail = newUser.Email;
+                return View("Add");
+            }
+        }
+
+    }
+}
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,41 @@
+﻿namespace SpaDay6.Models
+{
+    public class User
+    {
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public User(string username, string email, string password): this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+
+        public override string? ToString()
+        {
+            return Username;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is User user &&
+                   Id == user.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+    }
+}
