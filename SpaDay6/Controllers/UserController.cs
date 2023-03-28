acc3dfebfd8b307c784be352c5310754586bde1f
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-28 15:18:32 -0500

    studio as group 3/28

--- SpaDay6/Controllers/UserController.cs
+++ SpaDay6/Controllers/UserController.cs
@@ -10,24 +10,27 @@ namespace SpaDay6.Controllers
             return View();
         }
 
-        [HttpGet]
-        [Route("/user/add")]
         public IActionResult Add()
         {
-
 
             return View();
         }
 
+        [HttpPost("user")]
         public IActionResult SubmitAddUserForm(User newUser, string verify)
         {
             if (newUser.Password == verify)
             {
                 ViewBag.Username = newUser.Username;
-                return View();
+                return View("index");
             }
             else
             {
+                ViewBag.Username = newUser.Username;
+                ViewBag.Email = newUser.Email;
+                ViewBag.Password = newUser.Password;
+                ViewBag.verify = verify;
+                ViewBag.error = "Passwords must match!";
                 return View("add");
             }
         }
--- SpaDay6/Models/User.cs
+++ SpaDay6/Models/User.cs
@@ -2,17 +2,25 @@
 {
     public class User
     {
+        public int Id { get; }
+        public int nextId = 0;
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string VerifyPassword { get; set; }
+        //public string VerifyPassword { get; set; }
 
-        public User(string username, string email, string password, string verifyPassword)
+        public User()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public User(string username, string email, string password/*, string verifyPassword*/)
         {
             Username = username;
             Email = email;
             Password = password;
-            VerifyPassword = verifyPassword;
+            //VerifyPassword = verifyPassword;
         }
 
     }
