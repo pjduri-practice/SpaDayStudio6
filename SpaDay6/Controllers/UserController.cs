d61e258e62cc62443596501b69176a0215b43083
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-28 14:11:04 -0500

    added user class, add form, and controller template to add user

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+
+
+            return View();
+        }
+    }
+}
